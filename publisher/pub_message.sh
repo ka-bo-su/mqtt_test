10b380a43699efd98c06cbc5f3431699d6e45d75
ka-bo-su <kazuma_ikesaka@s.okayama-u.ac.jp>
2025-02-05 11:32:41 +0100

    通信の遅延を再現

--- publisher/pub_message.sh
+++ publisher/pub_message.sh
@@ -1,9 +1,10 @@
+tc qdisc add dev eth0 root netem delay 200ms 100ms distribution normal
 sleep 3
-count=$((RANDOM % 4 + 1))  # random number between 1 and 4 and this is the number of messages to be published
+count=3  # 3 messages will be published
 i=0
 
 while [ "$i" -lt "$count" ]; do
     mosquitto_pub -h mqtt-broker -t "test" -m "Hello, World!" -d | while read line; do echo "$(date "+%Y-%m-%d %H:%M:%S") $line" | tee -a /logs/mqtt-publisher.log; done
-    sleep 5
+    sleep 1
     i=$((i + 1))
 done
--- subscriber/sub_message.sh
+++ subscriber/sub_message.sh
@@ -1,13 +1,4 @@
-DURATION=25  # after 25 seconds, the subscriber will stop
+tc qdisc add dev eth0 root netem delay 200ms 100ms distribution normal
 sleep 2
 # use `mosquitto_sub` as a subscriber to the broker
-mosquitto_sub -h mqtt-broker -t "test" -d | while read line; do echo "$(date "+%Y-%m-%d %H:%M:%S") $line" | tee -a /logs/mqtt-subscriber.log; done &
-
-# get PID of `mosquitto_sub` process
-SUB_PID=$!
-
-# wait for the duration of the subscriber
-sleep "$DURATION"
-
-# kill the `mosquitto_sub` process
-kill "$SUB_PID"
+mosquitto_sub -h mqtt-broker -t "test" -d -C 3 | while read line; do echo "$(date "+%Y-%m-%d %H:%M:%S") $line" | tee -a /logs/mqtt-subscriber.log; done
