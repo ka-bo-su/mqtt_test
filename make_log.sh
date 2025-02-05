b4cee5a4bce307a9f11ccb00d9234090a41f4c07
ka-bo-su <kazuma_ikesaka@s.okayama-u.ac.jp>
2025-02-05 11:32:28 +0100

    最初のほうのログが取れていなかったので取れるように変更

--- make_log.sh
+++ make_log.sh
@@ -1,9 +1,9 @@
-docker-compose up -d  # launch the containers in the background
-docker-compose logs -f --tail=0 > ./logs/all_log.log &  # save the logs of all containers
-SUB_PID=$!
-for i in {30..1}; do
-    echo -n -e "\r${i} seconds remaining before stopping the subscriber"
+# docker-compose logs -f --tail=0 > ./logs/all_log.log &  # save the logs of all containers
+docker-compose up -d # launch the containers in the background
+docker-compose logs -f | tee ./logs/all_log.log &  # save the logs of all containers
+echo "$SUB_PID"
+for i in {8..1}; do
+    echo -n -e "\r${i} seconds remaining before stopping the container"
     sleep 1
 done
-kill "$SUB_PID"  # stop the log saving process
 docker-compose down  # stop the containers
